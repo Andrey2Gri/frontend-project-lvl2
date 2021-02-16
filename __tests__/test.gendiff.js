695cba20fd98bddfb5348241120283380efa2269
andrey <andrey2gri@gmail.com>
2021-02-16 19:34:18 +0300

    new test

--- __tests__/test.gendiff.js
+++ __tests__/test.gendiff.js
@@ -8,20 +8,34 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-let expected;
+let defaultResult;
+let plainResult;
 
 beforeAll(() => {
-  expected = readFile('expected');
+  defaultResult = readFile('defaultResult');
+  plainResult = readFile('plainResult');
 });
 
-test('gendiff', () => {
+test('gendiff default format', () => {
   const pathToJson1 = getFixturePath('file1.json');
   const pathToJson2 = getFixturePath('file2.json');
   const actualJson = gendiff(pathToJson1, pathToJson2);
-  expect(actualJson).toBe(expected);
+  expect(actualJson).toBe(defaultResult);
 
   const pathToYml1 = getFixturePath('file1.yml');
   const pathToYml2 = getFixturePath('file2.yml');
   const actualYml = gendiff(pathToYml1, pathToYml2);
-  expect(actualYml).toBe(expected);
+  expect(actualYml).toBe(defaultResult);
+});
+
+test('gendiff plain format', () => {
+  const pathToJson1 = getFixturePath('file1.json');
+  const pathToJson2 = getFixturePath('file2.json');
+  const actualJson = gendiff(pathToJson1, pathToJson2, 'plain');
+  expect(actualJson).toBe(plainResult);
+
+  const pathToYml1 = getFixturePath('file1.yml');
+  const pathToYml2 = getFixturePath('file2.yml');
+  const actualYml = gendiff(pathToYml1, pathToYml2, 'plain');
+  expect(actualYml).toBe(plainResult);
 });
